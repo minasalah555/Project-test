01e5b0487a55e8b3a8fa95d6cd2addf978c2168a
minasalah555 <salahmina452@gmail.com>
2022-01-01 12:53:12 +0200

    added bye eng

---
+++ src/newptogram/NewPtogram.java
@@ -0,0 +1,14 @@
+package newptogram;
+import java.util.Scanner;
+public class NewPtogram 
+{  
+    
+    public static void main(String[] args) 
+    {
+       int y=5;
+      char c='5';
+      int x=c;
+        System.out.println(x);
+        System.out.println("y+2 ="+ y+5);
+    }
+}
